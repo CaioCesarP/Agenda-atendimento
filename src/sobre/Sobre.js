e8db4993a9401e573dc56a6fdca565fbb0ff1b56
caio.cp <caio.cesar@itarget.com.br>
2022-04-25 12:14:17 -0300

    feat/carousel

--- src/sobre/Sobre.js
+++ src/sobre/Sobre.js
@@ -1,5 +1,17 @@
+import React from "react";
 import { Button, Typography } from "@mui/material";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 import "./style.css";
+
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const Sobre = () => {
   return (
@@ -25,43 +37,68 @@ const Sobre = () => {
           </div>
         </div>
         <div className="right">
-          <Typography variant="h4" color="red" gutterBottom>
-            COMENTÁRIOS
-          </Typography>
-          <div className="double">
-            <div className="card">
-              <p className="comentario">
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores ratione, ea dolor quidem autem et, tempora, sit non
-                nesciunt quas eligendi molestiae laudantium neque doloremque
-                voluptates quae perspiciatis sequi tenetur!{" "}
-              </p>
-              <p className="autor">CEO da Lorem ipsum</p>
-            </div>
-            <div className="card">
-              <p className="comentario">
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores ratione, ea dolor quidem autem et, tempora, sit non
-                nesciunt quas eligendi molestiae laudantium neque doloremque
-                voluptates quae perspiciatis sequi tenetur!{" "}
-              </p>
-              <p className="autor">Empresário Lorem ipsum</p>
-            </div>
-          </div>
-          <div className="single">
-            <div className="card">
-              <p className="comentario">
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores ratione, ea dolor quidem autem et, tempora, sit non
-                nesciunt quas eligendi molestiae laudantium neque doloremque
-                voluptates quae perspiciatis sequi tenetur!{" "}
-              </p>
-              <p className="autor">Equipe da Lorem ipsum</p>
-            </div>
-          </div>
+          <CarouselProvider
+            infinite
+            totalSlides={3}
+            naturalSlideHeight
+            naturalSlideWidth
+            isPlaying
+            interval={3000}
+            className="carousel"
+          >
+            <Slider>
+              <Slide index={0} className="card">
+                <p className="comentario">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Asperiores ratione, ea dolor quidem autem et, tempora, sit non
+                  nesciunt quas eligendi molestiae laudantium neque doloremque
+                  voluptates quae perspiciatis sequi tenetur!{" "}
+                </p>
+                <p className="autor">CEO da Lorem ipsum</p>
+                <img
+                  src="https://www.hublinksindia.com/img/sample.png"
+                  alt="cliente"
+                  className="img--client"
+                />
+              </Slide>
+              <Slide index={1} className="card">
+                <p className="comentario">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Asperiores ratione, ea dolor quidem autem et, tempora, sit non
+                  nesciunt quas eligendi molestiae laudantium neque doloremque
+                  voluptates quae perspiciatis sequi tenetur! ea dolor quidem
+                  autem et, tempora, sit.{" "}
+                </p>
+                <p className="autor">Empresário Lorem ipsum</p>
+                <img
+                  src="https://www.hublinksindia.com/img/sample.png"
+                  alt="cliente"
+                  className="img--client"
+                />
+              </Slide>
+              <Slide index={2} className="card">
+                <p className="comentario">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Asperiores ratione, ea dolor quidem autem et, tempora!{" "}
+                </p>
+                <p className="autor">Equipe da Lorem ipsum</p>
+                <img
+                  src="https://www.hublinksindia.com/img/sample.png"
+                  alt="cliente"
+                  className="img--client"
+                />
+              </Slide>
+            </Slider>
+            <ButtonBack className="button-slide">
+              <ArrowLeftIcon />
+            </ButtonBack>
+            <ButtonNext className="button-slide">
+              <ArrowRightIcon />
+            </ButtonNext>
+          </CarouselProvider>
         </div>
       </div>
     </div>
